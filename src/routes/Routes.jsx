4a37176bc3c0e3722b09c21fee39d0f5b42b1765
jkawamofc <kawamsilva1312@gmail.com>
2025-07-01 12:08:14 -0300

    alterações nas rotas

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom';
 
 import MainLayout from '../layouts/MainLayout';
-
 
 import Home from '../pages/Home';
 import Produtos from '../pages/Produtos';
@@ -19,31 +17,27 @@ import MeuPerfil from '../pages/pedidos/MeuPerfil';
 import MinhasInformacoes from '../pages/pedidos/MinhasInformaçoes';
 import Pagamento from '../pages/pedidos/Pagamento';
 import CompraFeita from '../pages/produtofinal/CompraFeita';
+
 function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route path="/" element={<MainLayout />}>
-
-          <Route index element={<Home />} />
-          <Route path="produtos" element={<Produtos />} />
-          <Route path="produtos/:id" element={<ProductDetailsPage />} />
-          <Route path="categorias" element={<Categorias />} />
-          <Route path="cadastro" element={<Cadastro />} />
-          <Route path="entrar" element={<Entrar />} />
-          <Route path="informacoes" element={<Informacoes />} />
-
-          <Route path="meus-pedidos" element={<MeusPedidos />} />
-          <Route path="compraprodutos" element={<CompraProdutos />} />
-          <Route path="finalizar-compra" element={<FinalizarCompra />} />
-          <Route path="meu-perfil" element={<MeuPerfil />} />
-          <Route path="minhas-informacoes" element={<MinhasInformacoes />} />
-          <Route path="pagamento" element={<Pagamento />} />
-          <Route path="compra-feita" element={<CompraFeita />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="produtos" element={<Produtos />} />
+        <Route path="produtos/:id" element={<ProductDetailsPage />} />
+        <Route path="categorias" element={<Categorias />} />
+        <Route path="cadastro" element={<Cadastro />} />
+        <Route path="entrar" element={<Entrar />} />
+        <Route path="informacoes" element={<Informacoes />} />
+        <Route path="meus-pedidos" element={<MeusPedidos />} />
+        <Route path="compraprodutos" element={<CompraProdutos />} />
+        <Route path="finalizar-compra" element={<FinalizarCompra />} />
+        <Route path="meu-perfil" element={<MeuPerfil />} />
+        <Route path="minhas-informacoes" element={<MinhasInformacoes />} />
+        <Route path="pagamento" element={<Pagamento />} />
+        <Route path="compra-feita" element={<CompraFeita />} />
+      </Route>
+    </Routes>
   );
 }
 
